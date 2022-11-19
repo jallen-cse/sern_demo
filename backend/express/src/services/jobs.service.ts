c7464e60c91b00a3d924ac9aa40d77c955b6edeb
Jack <jack.allen@above-uav.com>
2022-11-19 02:14:26 -0700

    implement first jobs and employees services

---
+++ backend/express/src/services/jobs.service.ts
@@ -0,0 +1,40 @@
+
+import { Op, Model } from "sequelize";
+
+import db from "./db";
+
+/**
+ * Get a job by ID.
+ * @param jobId job id to query
+ * @returns job if found, else null
+ */
+async function getJob(
+  jobId: number
+): Promise<Model<any, any> | null> {
+  return db.jobs.findByPk(jobId)
+}
+
+/**
+ * Get all jobs or jobs with given jobIds.
+ * @param jobIds job ids to query
+ * @returns array of jobs
+ */
+async function getJobs(
+  jobIds?: number[]
+): Promise<Model<any, any>[]> {
+  const options = jobIds ? 
+    {
+      where: {
+        id: {
+          [Op.in]: jobIds
+        }
+      }
+    } :
+    undefined;
+  return db.jobs.findAll(options);
+}
+
+export default {
+  getJob,
+  getJobs
+}
---
+++ backend/express/src/services/search.service.ts
@@ -0,0 +1,43 @@
+
+import { Model, Op } from "sequelize";
+
+import db from "./db";
+
+export interface SearchJobsArguments {
+  title: string
+}
+
+/**
+ * Construct the sequelize where clause to match title keywords.
+ * @param args SearchJobsArguments
+ * @returns sequelize find options
+ */
+function buildSearchWhereClause(args: SearchJobsArguments) {  
+  const titleTokens = args.title.split(' ').filter(v => v.length > 3);
+  return {
+    where: {
+      [Op.or]: titleTokens.map(kw => { 
+        return {
+          title: {
+            [Op.substring]: kw
+          }
+        }
+      })
+    }
+  };
+}
+
+/**
+ * Search for jobs like a given title.
+ * @param args SearchJobsArguments
+ * @returns array of jobs matching given search terms
+ */
+async function searchJobs(
+  args: SearchJobsArguments
+): Promise<Model<any, any>[]> {
+  return db.jobs.findAll(buildSearchWhereClause(args));
+}
+
+export default {
+  searchJobs
+}
