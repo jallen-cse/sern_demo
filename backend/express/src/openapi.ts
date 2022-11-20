b5881654ce868e9b2e0610c63880dda3a1fe617e
Jack <jack.allen@above-uav.com>
2022-11-20 02:05:01 -0700

    define job & error schemas for openapi

--- backend/express/src/openapi.ts
+++ backend/express/src/openapi.ts
@@ -1,6 +1,8 @@
 
 import swaggerJsdoc from "swagger-jsdoc";
 
+import { jobDTOSchema } from "./models/job.model";
+
 // parses inline JSDocs to generate OpenAPI document
 export default swaggerJsdoc({
   apis: ['./src/routes/*.ts'],
@@ -9,7 +11,27 @@ export default swaggerJsdoc({
     openapi: '3.0.0',
     info: {
       title: "Jack's Full Stack Demo",
-      version: '0.0.0',
+      version: '0.0.0'
+    },
+    components: {
+      schemas: {
+        Job: jobDTOSchema,
+        Error: {
+          type: "object",
+          properties: {
+            status: {
+              type: "integer",
+              description: "The HTTP status of the error",
+              required: true
+            },
+            message: {
+              type: "string",
+              description: "An indication of what went wrong",
+              required: true
+            }
+          }
+        }
+      }
     }
   }
 })
